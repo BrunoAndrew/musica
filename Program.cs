2dc95246d9f9ae7964aabeee0721a10f66ca2bb2
bruno <brunoandrewrocha@gmail.com>
2022-09-21 20:13:41 -0300

    soluçao do exercício

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("E meu chinelo havaiana");
+Console.WriteLine("Já pisou no barro, já pisou no asfalto");
+Console.WriteLine("Hoje eu piso no Golf Sportline");
+Console.WriteLine("Hoje eu fumo do porp, kurk ou ice");
+Console.WriteLine("Juntando muito dólar do spotify\n");
+
+Console.WriteLine("Isso que me motiva e eu caneto mais");
+Console.WriteLine("Agora minha meta é goma pro meus país");
+Console.WriteLine("Isso que me motiva e eu caneto mais");
+Console.WriteLine("Agora minha meta é casa pros meus pais\n");
+
+Console.WriteLine("Andar descalço no meu condomínio de juju");
+Console.WriteLine("Ma fuma fu, pé na piscina, tô tranquilão");
+Console.WriteLine("Hoje o maloqueiro curte a vida\n");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Eu vou morar na zona sul, vou tomar salute");
+Console.WriteLine("Andar descalço no meu condomínio de juju");
+Console.WriteLine("Ma fuma fu, pé na piscina, tô tranquilão");
+Console.WriteLine("Hoje os mandrake curte a vida\n");
+
+Console.ResetColor();
+Console.WriteLine("É engraçado né?");
+Console.WriteLine("Te jurei amor, você não me deu atenção");
+Console.WriteLine("Só por não ter dinheiro, não ter moto e nem carrão");
+Console.WriteLine("Se fosse bigode, passar cartão, vender droga");
+Console.WriteLine("Não era mesma fita do Paulin\n");
+
+Console.WriteLine("Nega, cê não gosta de mim, cê gosta do que eu vou bancar");
+Console.WriteLine("Se voar a de cem, sua calça desce para me dar");
+Console.WriteLine("Bora pro Atlântico, cê tá com o tubarão");
+Console.WriteLine("E no giro com o mandrake você tá na perdição\n");
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("Eu vou morar na zona sul, vou tomar salute");
+Console.WriteLine("Andar descalço no meu condomínio de juju");
+Console.WriteLine("Ma fuma fu, pé na piscina, tô tranquilão");
+Console.WriteLine("Hoje o maloqueiro curte a vida");
+Console.ResetColor();
